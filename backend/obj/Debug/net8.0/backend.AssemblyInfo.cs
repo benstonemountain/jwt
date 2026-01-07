72594956dec5ab0a5490865a2c9f4d7f5a9b8043
Kőhegyi Bence László <kohegyi.bence@hmei.hu>
2026-01-07 15:25:28 +0100

    backend shit

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241586ae997c1d7c79dba904386f96bd24e1852c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d812526d5f3fd55e2b8a50dd7377f841d5f311")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
