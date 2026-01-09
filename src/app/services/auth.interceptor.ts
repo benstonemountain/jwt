c2f197f8730d54434a619f4817b7ff64f809c4c9
Kőhegyi Bence László <kohegyi.bence@hmei.hu>
2026-01-09 20:53:35 +0100

    added comments to auth.service & auth.interceptor (cookie branch)

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -14,3 +14,8 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 //INTERCEPTOR (cookie)
 //Nincs Authorization header: a Backend most már a sütiből olvassa a tokent (amit a Program.cs-ben beállítottunk az OnMessageReceived eseménynél) --> ezért nem kell manuálisan belegyömöszölni a fejlécbe a Bearer <token>-t
 //withCredentials: true --> ez mondja meg az Angularnak, hogy "Kérlek, minden kérésnél, ami a backendre megy, engedd, hogy a böngésző automatikusan hozzácsapja a sütiket is!". Enélkül a süti nem menne el, és a szerver 401 Unauthorized hibát adna.
+
+//export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//req: Ez a kimenő HTTP kérés (pl. a GET vagy POST, amit épp indítasz). Ez az objektum „read-only” (csak olvasható), nem tudsz beleírni közvetlenül.
+//next: Ez egy függvény, ami azt mondja: „Kész vagyok, add tovább a kérést a következő láncszemnek (vagy ki a hálózatra)”.
+//mivel a req immutable/megváltozhatatlan, ezért klónozni kell 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -16,8 +16,38 @@ export class AuthService {
 	private roleKey = 'userRole';
 
 	/**
-	 * LOGIN
-	 * Fontos: withCredentials: true kell, hogy a böngésző elmentse a sütit!
+	 * REGISTER
+	 * A sütibe kerül a jwt, a frontend kód pedig nem tud belenézni a sütibe
+	 * a userRole az Angularnak kell, hogy frontenden tudjuk kezelni, hogy valaki admin vagy sima user
+	 * a message nem is kell igazából
+	 * FONTOS! Az, hogy én mit várok a backendtől, azt meg kell beszélnek a backendessel; jelenleg úgy van megírva a backend,
+	 * hogy visszaküldjön egy mesage-t, meg a user role:
+	 * return Ok(new { message = "Sikeres regisztráció", userRole = request.Role });
+	 */
+	register(credentials: FormInfos) {
+		return (
+			this.http
+				// A generikusban mondom meg az Angularnak, mire számítson a backendtől:
+				.post<{ message: string; userRole: string }>(
+					`${this.apiUrl}/register`,
+					credentials,
+					{
+						//ez engedélyezi a böngészőnek, hogy a szervertől érkező új sütit befogadja és elmentse
+						//ha ez nem lenne, a böngésző nem mentené el a tokent a sütibe
+						withCredentials: true,
+					},
+				)
+				.pipe(
+					tap((response) => {
+						//kiveszi a szerver válaszából a szerepkört (admin vagy user) és elmenti a localStorage-ba
+						localStorage.setItem(this.roleKey, response.userRole);
+					}),
+				)
+		);
+	}
+
+	/* LOGIN
+	Fontos: withCredentials: true kell, hogy a böngésző elmentse a sütit!
 	 */
 	login(credentials: FormInfos) {
 		return this.http
@@ -25,7 +55,9 @@ export class AuthService {
 				`${this.apiUrl}/login`,
 				credentials,
 				{
-					withCredentials: true, // KÖTELEZŐ a sütik kezeléséhez
+					//HttpOnly Cookie-s megoldás eseténe ez engedélyezi a böngészőnek, hogy a szervertől érkező új sütit befogadja és elmentse
+					//ha ez nem lenne, a böngésző nem mentené el a tokent a sütibe
+					withCredentials: true,
 				},
 			)
 			.pipe(
@@ -34,32 +66,14 @@ export class AuthService {
 					// Csak a role-t mentjük el, hogy tudjuk, mit mutassunk a menüben
 					localStorage.setItem(this.roleKey, response.userRole);
 
-					// Opcionálisan elmenthetjük a user adatait is, ha a backend küldi
+					// Opcionálisan elmenthetjük a user adatait is a localStorage-be, ha a backend küldi
 					localStorage.setItem(
 						'currentUser',
+						//a JSON.stringify: Azért kell, mert a localStorage csak szöveget tud tárolni
 						JSON.stringify({ id: credentials.username }),
 					);
 
 					console.log('Sikeres belépés. A token HttpOnly sütiben tárolódik.');
-				}),
-			);
-	}
-
-	/**
-	 * REGISTER
-	 */
-	register(credentials: FormInfos) {
-		return this.http
-			.post<{ message: string; userRole: string }>(
-				`${this.apiUrl}/register`,
-				credentials,
-				{
-					withCredentials: true,
-				},
-			)
-			.pipe(
-				tap((response) => {
-					localStorage.setItem(this.roleKey, response.userRole);
 				}),
 			);
 	}
@@ -76,13 +90,11 @@ export class AuthService {
 		return this.getRole() === 'admin';
 	}
 
-	/**
-	 * LOGOUT
-	 *
-	 */
+	/*LOGOUT*/
 	logout() {
 		// 1. Szólunk a szervernek, hogy semmisítse meg a sütit
 		this.http
+		//Mivel kijelentkezéskor nem küldünk adatot (nem kell felhasználónév vagy jelszó), de a post metódus elvárja, hogy legyen ott valami, egy üres objektumot {} adunk át az url után:
 			.post(`${this.apiUrl}/logout`, {}, { withCredentials: true })
 			.subscribe({
 				next: () => {
